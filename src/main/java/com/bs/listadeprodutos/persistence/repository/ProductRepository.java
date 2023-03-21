0b33de10af865d60e3aa10545a1890ca6066a6f7
bruno <brunosantoscomercial@hotmail.com>
2023-03-21 16:30:21 -0300

    Remove imports unused

--- src/main/java/com/bs/listadeprodutos/persistence/repository/ProductRepository.java
+++ src/main/java/com/bs/listadeprodutos/persistence/repository/ProductRepository.java
@@ -3,11 +3,9 @@ package com.bs.listadeprodutos.persistence.repository;
 import com.bs.listadeprodutos.persistence.entity.CompanyEntity;
 import com.bs.listadeprodutos.persistence.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
